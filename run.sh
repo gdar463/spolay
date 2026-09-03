e5e95086ad9793f5ad1ac76cb813b70881f52f99
gdar463 <dev@gdar463.com>
2026-09-03 22:32:23 +0200

    build: add option to refresh romfs and check for configure
    Signed-off-by: gdar463 <dev@gdar463.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+if [ ! -d build/linux ]; then
+    echo "Run configure.sh first."
+fi
+if [[ ${1,,} == "refresh_romfs" ]] && [ -f build/linux/third_party/romfs/lib/libromfs_resources.cpp ]; then
+    rm -f build/linux/third_party/romfs/lib/libromfs-spolay.a build/linux/third_party/romfs/lib/libromfs_resources.cpp
+    shift
+fi
 cmake --build build/linux -j $@
 build/linux/spolay
 
