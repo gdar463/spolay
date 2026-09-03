7a4aba1294ceb3743dbeeacc1d9aead1a8b40546
gdar463 <dev@gdar463.com>
2026-09-03 22:55:55 +0200

    build: add only_build option
    Signed-off-by: gdar463 <dev@gdar463.com>

--- run.sh
+++ run.sh
@@ -8,6 +8,14 @@ if [[ ${1,,} == "refresh_romfs" ]] && [ -f build/linux/third_party/romfs/lib/lib
     rm -f build/linux/third_party/romfs/lib/libromfs-spolay.a build/linux/third_party/romfs/lib/libromfs_resources.cpp
     shift
 fi
+if [[ ${1,,} == "only_build" ]]; then
+    only_build=1
+    shift
+else
+    only_build=0
+fi
 cmake --build build/linux -j $@
-build/linux/spolay
+if [ $only_build = 0 ]; then
+    build/linux/spolay
+fi
 
