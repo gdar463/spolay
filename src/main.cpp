705ff9695f2f40c036fb611635e22c1d37bca91f
gdar463 <dev@gdar463.com>
2026-09-04 17:52:03 +0200

    main: manage sdl events with a switch instead of a if
    Signed-off-by: gdar463 <dev@gdar463.com>

--- src/main.cpp
+++ src/main.cpp
@@ -93,11 +93,17 @@ int main() {
 		SDL_Event event;
 		while (SDL_PollEvent(&event)) {
 			ImGui_ImplSDL3_ProcessEvent(&event);
-			if (event.type == SDL_EVENT_QUIT) {
-				state.done = true;
-			}
-			if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(window)) {
-				state.done = true;
+			switch (event.type) {
+				case SDL_EVENT_QUIT:
+					state.done = true;
+					break;
+				case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
+					if (event.window.windowID == SDL_GetWindowID(window)) {
+						state.done = true;
+					}
+					break;
+				default:
+					[[likely]] break;
 			}
 		}
 
